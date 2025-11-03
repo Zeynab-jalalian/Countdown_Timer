f1c928f4dbf52d2f3afc4f3a9a2f7ca95518ca4d
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-11-03 09:13:57 +0330

    time

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+const dayEl = document.getElementById("days");
+const hourEl = document.getElementById("hours");
+const minEl = document.getElementById("mins");
+const secondEl = document.getElementById("seconds");
+
 const newYears = "1 Jan 2026";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
-  const seconds=(newYearsDate - currentDate) / 1000; 
-  const days=Math.floor(seconds / 3600 / 24);
-  const hours=Math.floor(seconds / 3600) % 24;
-  const mins=Math.floor(seconds / 60) % 60;
-  const second=Math.floor(seconds) % 60;
+  const seconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(seconds / 3600 / 24);
+  const hours = Math.floor(seconds / 3600) % 24;
+  const mins = Math.floor(seconds / 60) % 60;
+  const second = Math.floor(seconds) % 60;
+
+  dayEl.innerHTML=days;
+  hourEl.innerHTML=hours;
+  minEl.innerHTML=mins;
+  secondEl.innerHTML=second;
 }
 
 //initial call
 countdown();
 
-setInterval(countdown,1000);
+setInterval(countdown, 1000);
